40d42b3c577e3c2e0c79632f870061cf7ea5cb52
Li Pu <825369365@qq.com>
2023-04-09 07:02:16 +0000

    feat(page): 基本钟表结构实现

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div v-for="clock in routeMap" :key="clock.link">
+      <NuxtLink :to="`/${clock.link}`">{{ clock.title }}</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const routeMap = [
+  {
+    link: 'base',
+    title: '基本钟表',
+  }
+]
+</script>
+
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+</style>
