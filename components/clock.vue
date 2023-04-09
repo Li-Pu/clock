8f97e9e90c5d92a5e72d5907a581240ea9c4230a
Li Pu <825369365@qq.com>
2023-04-09 08:24:55 +0000

    feat(component): 使用tailwindcss样式

--- components/clock.vue
+++ components/clock.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="clock-container">
-        <div class="clock-content">
-            <div class="clock-text">
-                <span class="time-text hour">{{ timeNumberFormat(time.hour) }}</span>
-                <span class="gap">{{ time.second % 2 === 0 ? '' : ':' }}</span>
-                <span class="time-text minute">{{ timeNumberFormat(time.minute) }}</span>
-                <span class="gap">{{ time.second % 2 === 0 ? '' : ':' }}</span>
-                <span class="time-text second">{{ timeNumberFormat(time.second) }}</span>
+    <div class="w-full h-full">
+        <div class="w-full h-full flex items-center justify-center">
+            <div class="font-mono text-9xl flex flex-row text-center">
+                <span>{{ timeNumberFormat(time.hour) }}</span>
+                <span :class="{ 'invisible': time.second % 2 === 0 }">:</span>
+                <span>{{ timeNumberFormat(time.minute) }}</span>
+                <span :class="{ 'invisible': time.second % 2 === 0 }">:</span>
+                <span>{{ timeNumberFormat(time.second) }}</span>
             </div>
         </div>
     </div>
@@ -15,34 +15,3 @@
 <script lang="ts" setup>
 const time = useTime();
 </script>
-
-<style scoped>
-.clock-container {
-    width: 100vw;
-    height: 100vh;
-}
-
-.clock-content {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.clock-text {
-    font-size: 100px;
-}
-
-.clock-text .gap {
-    display: inline-block;
-    width: 0.5em;
-    text-align: center;
-    vertical-align: baseline;
-}
-
-.clock-text .time-text {
-    display: inline-block;
-    width: 1.2em;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="w-screen h-screen flex flex-row items-center justify-center">
     <div v-for="clock in routeMap" :key="clock.link">
       <NuxtLink :to="`/${clock.link}`">{{ clock.title }}</NuxtLink>
     </div>
@@ -14,14 +14,3 @@ const routeMap = [
   }
 ]
 </script>
-
-<style scoped>
-.container {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-}
-</style>
